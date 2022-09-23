1d8b8aa6febe8b63152952f518e79cb98b0d60f4
DrakoSk <113952438+DrakoSk@users.noreply.github.com>
2022-09-23 10:40:52 -0500

    Update sistema_01.py

--- sistema_01.py
+++ sistema_01.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
@@ -30,6 +31,27 @@ def limpiarCampos():
 	miCargo.set("")
 	miSalario.set("")
 
+def mostrar():
+	miConexion=sqlite3.connect("base")
+	miCursor=miConexion.cursor()
+	registro=tree.get_children()
+	for elemento in registro:
+		tree.delete(elemento)
+	try:
+		miCursor.execute("SELECT * FROM empleado")
+		for fila in miCursor:
+			tree.insert("",0,text=fila[0], values=(fila[1],fila[2],fila[3]))
+	except:
+		pass
+
+def elimanar():
+	miConexion=sqlite3.connect("base")
+	miCursor=miConexion.cursor()
+	if miCursor.askyesno(message="Datos seran eliminados permanentemente",title="Advertencia"):
+		miCursor.execute("DROP TABLE empleado")
+	else:
+		pass
+
 def crear():
 	miConexion=sqlite3.connect("base")
 	miCursor=miConexion.cursor()
@@ -42,3 +64,22 @@ def crear():
 		pass
 	limpiarCampos()
 	mostrar()
+
+# dibujar tabla 
+tree.tkk.Treeview(height=10, colmns=('#0','#1','#2'))
+tree.place(x=0,y=130)
+tree.column('#0', width=180)
+tree.heading('#0', text='ID', anchor=CENTER)
+tree.heading('#1', text='Nombres', anchor=CENTER)
+tree.heading('#2', text='Cargo', anchor=CENTER)
+tree.column('#3', width=180)
+tree.heading('#3', text='Salario', anchor=CENTER)
+
+def seleccionar(event):
+	item=tree.identify('item', event.x event.y)
+	miId.set(tree.item(item,'text'))
+	miNombre.set(tree.item(item,'Values')[0])
+	miCargo.set(tree.item(item,'Values')[1])
+	miSalario.set(tree.item(item,'Values')[2])
+
+tree.bind("<Dou")
